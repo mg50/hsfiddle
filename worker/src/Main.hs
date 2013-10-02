4fb2d2664b9a8b6a500ff42f705ce18730acdfd5
Mark Grant <mark@8thlight.com>
2013-10-01 19:21:25 -0500

    Fix worker compilation

--- worker/src/Main.hs
+++ worker/src/Main.hs
@@ -4,26 +4,31 @@ module Main where
 import Network.AMQP
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TLazy
+import qualified Data.Text.Lazy.Encoding as Enc
 import Compile
 
 main = do
   conn <- openConnection "127.0.0.1" "/" "guest" "guest"
   chan <- openChannel conn
 
-  consumeMsgs chan "uncompiled" Ack (compile chan)
+  consumeMsgs chan "uncompiled" Ack (tryCompile chan)
   getLine
   closeConnection conn
   putStrLn "connection closed"
 
-compile chan (requestMsg, envelope) =
+tryCompile chan (requestMsg, envelope) =
   case msgID requestMsg of
     Nothing -> return ()
     Just requestId -> do
-      result <- compile msg
+      result <- compile $ lazyBytestringToText $ msgBody requestMsg
       let (txt, queue) = case result of
                            CompileSuccess js -> (js, "compiled")
                            CompileError err  -> (err, "error")
-          replyMsg = newMsg{ msgBody = txt
+          replyMsg = newMsg{ msgBody = textToLazyBytestring txt
                            , msgDeliveryMode = Just Persistent
                            , msgReplyTo = Just requestId }
       publishMsg chan "hsfiddle" queue replyMsg
+
+lazyBytestringToText = TLazy.toStrict . Enc.decodeUtf8
+textToLazyBytestring = Enc.encodeUtf8 . TLazy.fromStrict
